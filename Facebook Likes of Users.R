909bd0329c14ebbe80281595ce26c3f86bc7855c
minimaxir <max@minimaxir.com>
2012-12-30 00:37:20 -0800

    Update Facebook Likes of Users.R

--- Facebook Likes of Users.R
+++ Facebook Likes of Users.R
@@ -1,9 +1,9 @@
 library(rjson)
 
-username <- "max.woolf"
-access_token <- "INSERT TOKEN HERE"
+username <- "INSERT FACEBOOK USERNAME HERE"
+access_token <- "INSERT ACCESS TOKEN HERE"
 
-nrows <- 5000
+nrows <- 5000 # max number of friends
 
 data<- data.frame(b=rep(NA,nrows), c=rep(NA,nrows),d=rep(NA,nrows))
 
@@ -44,7 +44,10 @@ numFriends <- floor(length(JSONfriends[[1]]))
 data <- na.omit(data)
 
 data[,3] <- as.numeric(data[,3])
-data<-data[with(data, order(-d,b)), ]
+data<-data[with(data, order(-d,b)), ] # sorts Likes, descending
 
 names(data) <- c("Name","Profile URL", "# Likes")
-write.table(data, paste(username,"-FriendLikeCounts.txt",sep=""),sep="\t",row.names=F,qmethod="d")
+write.table(data, paste(username,"-FriendLikeCounts.txt",sep=""),sep="\t",row.names=F,qmethod="d") # tab-delimited text file, easily opens in Excel
+
+# If you want a bonus histogram of the likes:
+# hist(data[,3], n=500, main=paste("Distribution of " ,username,"'s friends' Facebook Likes",sep=""), xlab="Likes", col="red", xlim=c(0,1000))
